cacd9468f625648037f1606a7291fa92bcf2364b
codefuldom <dom@codefuldom.com>
2020-03-10 00:05:12 -0500

    :rocket: add area formula to numbers.js

---
+++ section-2/numbers.js
@@ -0,0 +1,9 @@
+const age = 37
+console.log(age)
+
+let radius = 10
+let pi = 3.14
+console.log(`Radius: ${radius}, Pi: ${pi}`)
+
+let area = pi * radius ** 2
+console.log('%c%s', 'color: #917399', `area: ${area}`)
