8906bb88e4c71753dd2a95e15b3e1b462a580d1a
codefuldom <dom@codefuldom.com>
2020-03-09 23:59:32 -0500

    :sparkles: add lastIndexOf, slice, substring methods to examples

--- section-2/string.js
+++ section-2/string.js
@@ -26,3 +26,15 @@ console.log('%c%s', 'color: #aa00ff', `replace: ${replace}`)
 
 let value = email.indexOf('@')
 console.log('%c%s', 'color: #e50000', `index of value: ${value}`)
+
+// lastIndexOf - find the last instance of searched value
+let myEmail = email.lastIndexOf('n')
+console.log('%c%s', 'color: #733d00', `my email: ${myEmail}`)
+
+// slice - chop from to end position passed as args
+let slicedEmail = email.slice(0, 4)
+console.log('%c%s', 'color: #00bf00', `slicedEmail: ${slicedEmail}`)
+
+// substring - create a substring from org object. Start -> Get up to char
+let substring = email.substr(0, 3)
+console.log('%c%s', 'color: #0088cc', `substring: ${substring}`)
