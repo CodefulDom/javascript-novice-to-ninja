c26469274a33428cb57d2edc6e2db93316f18a1e
codefuldom <dom@codefuldom.com>
2020-03-10 00:11:12 -0500

    :sparkles: work with numbers in js

--- section-2/numbers.js
+++ section-2/numbers.js
@@ -7,3 +7,16 @@ console.log(`Radius: ${radius}, Pi: ${pi}`)
 
 let area = pi * radius ** 2
 console.log('%c%s', 'color: #917399', `area: ${area}`)
+
+// order of operations - BIDMAS
+/* 
+B - Bracket
+I  - Indices(Exponents)
+D - Division
+M - Multiplication
+A - Addition
+S - Subtraction
+*/
+
+let likes = 1
+console.log('%c%s', 'color: #d90000', `likes: ${(likes += 20)}`)
