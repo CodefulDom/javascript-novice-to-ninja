19e803fb08fce81e8164cf75f34f66be2d6a3a3e
codefuldom <dom@codefuldom.com>
2020-03-09 23:33:56 -0500

    :rocket: start variables, constants, and comments module

--- section-2/index.js
+++ section-2/index.js
@@ -1,2 +1,6 @@
-console.log(`test`)
-alert('HEY!')
+let age = 37
+let year = 2020
+console.log(`Dom's year of birth: ${year - age} `)
+
+const points = 100
+console.log('%c%s', 'color: #f2ceb6', points)
