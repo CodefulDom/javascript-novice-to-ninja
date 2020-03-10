eea097d70b20a2c4c04b2eaa38e9e5d25fd09f56
codefuldom <dom@codefuldom.com>
2020-03-09 23:36:20 -0500

    :rocket: complete variables constants and comments module

--- section-2/index.js
+++ section-2/index.js
@@ -3,4 +3,11 @@ let year = 2020
 console.log(`Dom's year of birth: ${year - age} `)
 
 const points = 100
-console.log('%c%s', 'color: #f2ceb6', points)
+console.log('%c%s', 'color: #f2ceb6', `points: ${points}`)
+
+// single line comments
+/* 
+Multi
+Line
+Comment
+*/
