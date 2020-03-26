4a379b407e40f0d3cdbaa308a0ddcddff0f01f64
codefuldom <dom@codefuldom.com>
2020-03-26 17:12:34 -0500

    :rocket: refactor code into a switch statement from an if statement

---
+++ section-3/switch.js
@@ -0,0 +1,35 @@
+const getGrade = (grade) => {
+  if (grade >= 90) {
+    console.log(`Your grade is an A`)
+  } else if (grade >= 80) {
+    console.log(`Your grade is a B`)
+  } else if (grade >= 70) {
+    console.log(`Your grade is a C`)
+  } else if (grade >= 60) {
+    console.log(`Your grade is a D`)
+  } else if (grade >= 50) {
+    console.log(`Your grade is a F`)
+  }
+}
+
+getGrade(50) /* ? */
+
+let grade = 'A'
+switch (grade) {
+  case 'A':
+    console.log(`Your grade is an A`)
+    break
+  case 'B':
+    console.log(`Your grade is an B`)
+    break
+  case 'C':
+    console.log(`Your grade is an C`)
+    break
+  case 'D':
+    console.log(`Your grade is an D`)
+    break
+  case 'F':
+    console.log(`You have a failing grade`)
+  default:
+    console.log('Not a Valid grade')
+}
